70530fe2faa8804a0cb1a9f6efb2b9d619f2c01a
xixn2 <ssbkh0802@naver.com>
2023-08-25 14:02:18 +0900

    Initial Commit

---
+++ MemoAppSwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MemoAppSwift
+//
+//  Created by 서지완 on 2023/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoAppSwift/MemoAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppSwiftApp.swift
+//  MemoAppSwift
+//
+//  Created by 서지완 on 2023/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
